2286a515841f9e6449a4dd26ae8d99a160b17b09
Vic18256 <vherkan@hotmail.co.uk>
2018-11-25 14:05:40 +0000

    1st commit quiz programme

---
+++ quiz/quiz.py
@@ -0,0 +1,9 @@
+def show_menu():
+  print("1. Ask a question")
+  print("2. Add a question")
+  print("3. Exit game")
+  
+  option = input ("Enter option: ")
+  return option
+  
+print(show_menu())
